5bc1520ed3880353b80f07336ec898187c2cbbf7
Mihael Filic <you@example.com>
2025-11-30 17:21:54 +0100

    Izmjenjen kod

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,8 @@
 package app;
-
 import entities.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.*;
-
 /**
  * Glavna klasa aplikacije
  */
@@ -28,43 +25,37 @@ public class Main {
             vozaci.add(ucitajVozace(sc));
         }
 //        ispisiVozace(vozaci);
-//        for (Integer i = 0; i <Konstante.POLJA.getValue(); i++) {
-//            kondukteri.add(ucitajKonduktere(sc));
-//        }
-//        for (Driver d : vozaci) {
-//            potraga.add(new Driver.DriverBuilder()
-//                    .setName(d.getName())
-//                    .setSurname(d.getSurname())
-//                    .setAge(d.getAge())
-//                    .setUsername(d.getUsername())
-//                    .setPassword(d.getPassword())
-//                    .setRole(d.getRole())
-//                    .build());
-//            brojac++;
-//        }
-//        for (Conductor k : kondukteri) {
-//            potraga.add(new Conductor(k.getName(), k.getSurname(), k.getAge()));
-//            brojac++;
-//            ((Conductor) potraga.get(brojac - 1)).setUsername(k.getUsername());
-//            ((Conductor) potraga.get(brojac - 1)).setPassword(k.getPassword());
-//            ((Conductor) potraga.get(brojac - 1)).setNumberOfAssignedVehicles(k.getNumberOfAssignedVehicles());
-//        }
-//        najmladi(potraga);
+        for (Integer i = 0; i <Konstante.POLJA.getValue(); i++) {
+            kondukteri.add(ucitajKonduktere(sc));
+        }
+        for (Driver d : vozaci) {
+            potraga.add(new Driver.DriverBuilder()
+                    .setName(d.getName())
+                    .setSurname(d.getSurname())
+                    .setAge(d.getAge())
+                    .setUsername(d.getUsername())
+                    .setPassword(d.getPassword())
+                    .setRole(d.getRole())
+                    .build());
+            brojac++;
+        }
+        for (Conductor k : kondukteri) {
+            potraga.add(new Conductor(k.getName(), k.getSurname(), k.getAge()));
+            brojac++;
+            ((Conductor) potraga.get(brojac - 1)).setUsername(k.getUsername());
+            ((Conductor) potraga.get(brojac - 1)).setPassword(k.getPassword());
+            ((Conductor) potraga.get(brojac - 1)).setNumberOfAssignedVehicles(k.getNumberOfAssignedVehicles());
+        }
+        najmladi(potraga);
         for (Integer i = 0; i <Konstante.POLJA.getValue() ; i++) {
             vozila.add(ucitajVozila(sc, vozaci));
         }
-//        ispisiVozila(vozila);
         for (Integer i = 0; i < Konstante.POLJA.getValue(); i++) {
             rute.add(ucitajRute(sc,vozila));
         }
-//        ispisiRute(rute);
-//        for (Integer i = 0; i < Konstante.POLJA.getValue(); i++) {
-//            putnici.add(upisiPutnike(sc));
-//        }
-
-
-
-
+        for (Integer i = 0; i < Konstante.POLJA.getValue(); i++) {
+            putnici.add(upisiPutnike(sc));
+        }
         System.out.println("Upisi 1 za pronalazak info o vozacima na određenoj ruti ili 0 za daljnje pretrazivanje: ");
         while (true) {
             try {
@@ -82,32 +73,23 @@ public class Main {
         }
         sc.nextLine();
         InfoVozac(provjera,sc,rute);
-        Route najkraca=najkracaRuta(rute);
-        System.out.println("Najkraca ruta je od "+najkraca.getStartStation()+" do "+najkraca.getEndStation()+" sa "+najkraca.getNumberOfStops()+" stanica.");
-        Vehicle najmanji=najmKapacitet(vozila);
-        System.out.println("Vozilo sa najmanjim kapacitetom je registracije "+najmanji.getRegistrationNumber()+" sa kapacitetom od "+najmanji.getCapacity()+".");
+        Optional<Route> najkraca=najkracaRuta(rute);
+        if (najkraca.isPresent()) {
+            log.info("Najkraca ruta je pronadena.");
+            System.out.println("Najkraca ruta je od "+najkraca.get().getStartStation()+" do "+najkraca.get().getEndStation()+" sa "+najkraca.get().getNumberOfStops()+" stanica.");
+
+        } else {
+            log.warn("Nije pronadena najkraca ruta.");
+        }
+        Optional<Vehicle> najmanji=najmKapacitet(vozila);
+        if (najmanji.isPresent()) {
+            log.info("Vozilo sa najmanjim kapacitetom je pronadeno.");
+            System.out.println("Vozilo sa najmanjim kapacitetom je registracije "+najmanji.get().getRegistrationNumber()+" sa kapacitetom od "+najmanji.get().getCapacity()+".");
+        } else {
+            log.warn("Nije pronadeno vozilo sa najmanjim kapacitetom.");
+        }
         sc.close();
     }
-
-
-
-//    private static Person ucitajOsobe(Scanner sc) {
-//        String ime;
-//        String prezime;
-//        Integer godine;
-//        Person osoba;
-//        System.out.println("Unesite ime osobe:");
-//        ime=sc.nextLine();
-//        System.out.println("Unesite prezime osobe:");
-//        prezime=sc.nextLine();
-//        System.out.println("Unesite godine osobe:");
-//        godine=sc.nextInt();
-//        osoba=new Person(ime,prezime,godine);
-//        sc.nextLine();
-//        return osoba;
-//    }
-
-
     /**
      * provjerava ispravnost broja
      *
@@ -139,14 +121,12 @@ public class Main {
      * @param ime ime za provjeru
      * @throws InvalidNameException iznimka koja se baca ako ime nije ispravno
      */
-
     private static void provjeraName(String ime) throws InvalidNameException {
         if (!ime.matches("^[A-ZČĆŽŠĐ][a-zčćžšđ]+$")) {
             throw new InvalidNameException("Ime mora pocinjati velikim slovom te sva sljedeca slova " +
                     "moraju biti mala. Ne smije sadrzavati brojeve ili posebne znakove. Pokusajte ponovno.");
         }
     }
-
     /**
      * učitava vozace
      *
@@ -214,8 +194,6 @@ public class Main {
                 .setRole(role)
                 .build();
     }
-
-
     /**
      * učitava konduktere
      *
@@ -224,7 +202,6 @@ public class Main {
      * @param sc scanner za unos podataka
      * @return objekt konduktera
      */
-
     private static Conductor ucitajKonduktere(Scanner sc) {
         log.info("Ucitaj konduktere metoda je pokrenuta.");
         String name;
@@ -249,7 +226,6 @@ public class Main {
         log.info("Ucitaj konduktere metoda je zavrsena.");
         return kondukter;
     }
-
     /**
      * upisuje putnike
      *
@@ -258,7 +234,6 @@ public class Main {
      * @param sc scanner za unos podataka
      * @return objekt putnika
      */
-
     private static Passenger upisiPutnike(Scanner sc){
         String name;
         String surname;
@@ -276,8 +251,6 @@ public class Main {
         System.out.println();
         return new Passenger(name,surname,age,ticketNumber);
     }
-
-
     /**
      * učitava vozila
      *
@@ -287,7 +260,6 @@ public class Main {
      * @param vozaci vozaci koji se mogu dodijeliti vozilu
      * @return objekt vozila
      */
-
     private static Vehicle ucitajVozila(Scanner sc,Set<Driver> vozaci) {
         log.warn("Niz vozaca bi mogao biti prazan ako prethodno nije unesen niti jedan vozac.");
         Integer ponovno = 0;
@@ -339,8 +311,6 @@ public class Main {
         }
         return vozilo;
     }
-
-
     /**
      * učitava rute
      * <p>
@@ -351,7 +321,6 @@ public class Main {
      * @param vozila vozila dodijeljena ruti
      * @return objekt rute
      */
-
     private static Route ucitajRute(Scanner sc,Set<Vehicle> vozila){
         log.warn("Niz vozila bi mogao biti prazan ako prethodno nije uneseno niti jedno vozilo.");
         Integer ponovo = 0;
@@ -397,8 +366,6 @@ public class Main {
         }
         return ruta;
     }
-
-
     /**
      * ispisuje informacije o vozacima na određenoj ruti
      *
@@ -436,135 +403,47 @@ public class Main {
             }
         }
     }
-
-//    private static void ispisiOsobe(Person[] osobe) {
-//        for (Integer i = 0; i < osobe.length; i++) {
-//            System.out.println(osobe[i].getName() + " " + osobe[i].getSurname() + " " + osobe[i].getAge());
-//            System.out.println();
-//
-//        }
-//    }
-
-//   private static void ispisiVozace(Driver[] vozaci) {
-//       for (Integer i = 0; i < vozaci.length; i++) {
-//           System.out.println(vozaci[i].getName() + " " + vozaci[i].getSurname() + " " + vozaci[i].getAge());
-//           System.out.println(vozaci[i].getUsername() + " " + vozaci[i].getPassword()+" " +vozaci[i].getRole());
-//           System.out.println();
-//        }
-//    }
-
-//    private static void ispisiRute(Route[] rute){
-//        for (Integer i = 0; i < rute.length ; i++) {
-//            System.out.println(rute[i].getStartStation()+" - "+rute[i].getEndStation()+" "+rute[i].getNumberOfStops());
-//        }
-//    }
-//    private static void ispisiVozila(Vehicle[] vozila) {
-//        for(int i=0;i<vozila.length;i++){
-//            System.out.println(vozila[i].getRegistrationNumber()+" "+vozila[i].getType()+" "+vozila[i].getCapacity());
-//        }
-//    }
-
-
+    private static void ispisljudi(List<? extends Person> lista){
+        for (Person t : lista) {
+            System.out.println(t.getName()+" "+t.getSurname()+" "+t.getAge());
+            System.out.println("\n");
+        }
+    }
     /**
      * pronalazi najkraću rutu
      *
      * @param rute rute među kojima se traži najkraća
      * @return najkraća ruta
      */
-    private static Route najkracaRuta(List<Route> rute){
-        Route najkraca=rute.getFirst();
-        Integer najmanji=rute.getFirst().getNumberOfStops();
-        for(Route r:rute){
-            if(r.getNumberOfStops()<najmanji){
-                najmanji=r.getNumberOfStops();
-                najkraca=r;
-            }
-        }
+    private static Optional<Route> najkracaRuta(List<Route> rute){
+        Optional<Route> najkraca=rute.stream()
+                .min(Comparator.comparingInt(Route::getNumberOfStops));
         return najkraca;
     }
-
-//    private static Route najduljaRuta(Route[] rute){
-//        Route najdulja;
-//        Integer najduljaId=0;
-//        Integer najveci=rute[0].getNumberOfStops();
-//        for(int i=0;i<rute.length;i++){
-//            if(rute[i].getNumberOfStops()>najveci){
-//                najveci=rute[i].getNumberOfStops();
-//                najduljaId=i;
-//            }
-//        }
-//        najdulja=rute[najduljaId];
-//        return najdulja;
-//    }
-
-
     /**
      * pronalazi vozilo sa najmanjim kapacitetom
      *
      * @param vozila vozila među kojima se traži
      * @return vozilo sa najmanjim kapacitetom
      */
-
-    private static Vehicle najmKapacitet(Set<Vehicle> vozila){
-        Vehicle najmKapacitet=null;
-        Integer najmanji=10000;
-        for(Vehicle v:vozila){
-            if(v.getCapacity()<najmanji){
-                najmanji=v.getCapacity();
-            }
-        }
-        for(Vehicle v:vozila){
-            if(v.getCapacity().equals(najmanji)){
-                najmKapacitet=v;
-                break;
-            }
-        }
-        return najmKapacitet;
+    private static Optional<Vehicle> najmKapacitet(Set<Vehicle> vozila){
+        Optional<Vehicle> najm=vozila.stream()
+                .min(Comparator.comparingInt(Vehicle::getCapacity));
+        return najm;
     }
-
-//    private static Vehicle najvKapacitet(Vehicle[] vozila){
-//        Vehicle najvKapacitet;
-//        Integer najvKapacitetId=0;
-//        Integer najveci=vozila[0].getCapacity();
-//        for(int i=0;i<vozila.length;i++){
-//            if(vozila[i].getCapacity()>najveci){
-//                najveci=vozila[i].getCapacity();
-//                najvKapacitetId=i;
-//            }
-//        }
-//        najvKapacitet=vozila[najvKapacitetId];
-//        return najvKapacitet;
-//    }
-//    private static Driver najstarijiVozac(Driver[] vozaci){
-//        Driver najstr;
-//        Integer najstrId=0;
-//        Integer najstariji=vozaci[0].getAge();
-//        for(int i=0;i<vozaci.length;i++){
-//            if(vozaci[i].getAge()>najstariji){
-//                najstariji=vozaci[i].getAge();
-//                najstrId=i;
-//            }
-//        }
-//        najstr=vozaci[najstrId];
-//        return najstr;
-//    }
     /**
      * pronalazi najmlađeg vozača
      *
      * @param trazi niz osoba među kojima se traži najmlađi vozač
      */
     private static void najmladi(List<Person> trazi){
-        Driver najml=null;
         log.warn("Niz osoba bi mogao biti prazan ako prethodno nije unesen niti jedan vozac ili kondukter.");
-        Integer najmladi=150;
-        for(Person p:trazi){
-            if (p instanceof Driver k) {
-                if (k.getAge()<najmladi){
-                    najmladi=k.getAge();
-                    najml=k;
-                }
-            }
-        }
-        System.out.println("Najmladi vozac je "+najml.getName()+" "+najml.getSurname()+" sa "+najml.getAge()+" godina.");
+        Optional<Driver> najml=trazi.stream()
+                .filter(d-> d instanceof Driver)
+                .map(d->(Driver) d)
+                .min(Comparator.comparingInt(Driver::getAge));
+
+        najml.ifPresent(driver -> System.out.println("Najmladi vozac je " + driver.getName() + " " + driver.getSurname() + " sa " + driver.getAge() + " godina."));
+
     }
 }
